e2d1cbe53ce0f83b7e7c01189821f419a33b3c18
Jirayut Najan <tonklaeiei123@gmail.com>
2025-03-13 20:21:35 +0700

    createCheckoutSession controller +

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import {protectRoute} from "../middleware/auth.middleware.js"
+import { checkout, createCheckoutSession } from "../controllers/payment.controller.js"
 
 const router = express.Router();
 
-router.post("/create-checkout-session")
+router.post("/create-checkout-session", protectRoute, createCheckoutSession);
+
+router.post("/checkout-success", protectRoute, checkout)
 
 export default router;
